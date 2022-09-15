7da89c0b40299eaf5110437be7b49e4e79a1ded5
mironshoh1603 <mironshohasadov1603@gmail.com>
2022-09-15 12:18:07 +0500

    controller folfer name is changed

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const userController = require('./../controllers/userController');
-const authController = require('./../controllers/authController');
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
