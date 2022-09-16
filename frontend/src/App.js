317844785bceff44d7a9b18b4af9c69feb4d8a6d
Umid Rustamov <rustamov.umidbek@gmail.com>
2022-09-16 19:39:03 +0500

    Frontend deyarli tayyor reactda

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import SignIn from "./components/Auth/SignIn";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Contact from "./components/Contact";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+
+      <Hero />
+
+      <Footer />
     </div>
   );
 }
