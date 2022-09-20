54c14d1b59fb471a0e85441c3b2add8d603162f9
Jamshid <kalandarovjamshid01@gmail.com>
2022-09-20 20:45:11 +0500

    home page and contact page is end

--- Controllers/viewController.js
+++ Controllers/viewController.js
@@ -1,8 +1,40 @@
+const axios = require('axios').default;
+
 const home = async (req, res, next) => {
   try {
-    res.render('home');
+    const books = await (
+      await axios.get('http://127.0.0.1:8000/api/v1/files/')
+    ).data.data;
+    const { data } = await axios('http://localhost:8000/api/v1/users/');
+    const teachers = data.data.filter((word) => word.role === 'teacher');
+    let newArr = [];
+    const subjects = teachers.map((val) => {
+      let variable = val.subjects[0] || { name: '' };
+      newArr.push(variable.name);
+    });
+    console.log(newArr, 'mana subjects');
+    const mavzular = await (
+      await axios.get('http://localhost:8000/api/v1/subjects/')
+    ).data.data;
+    console.log(books.length);
+    res.render('home', {
+      books,
+      teachers,
+      mavzular,
+      newArr,
+    });
   } catch (error) {
     console.log(error);
   }
 };
-module.exports = { home };
+
+const contact = async (req, res, next) => {
+  try {
+    res.render('contact');
+    console.log('hello');
+  } catch (err) {
+    res.render('error');
+    console.log('salom');
+  }
+};
+module.exports = { home, contact };
--- routes/view_route.js
+++ routes/view_route.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const viewController = require('./../Controllers/viewController');
+router.use('/contact', viewController.contact);
 
 router.route('/home').get(viewController.home);
-
 module.exports = router;
