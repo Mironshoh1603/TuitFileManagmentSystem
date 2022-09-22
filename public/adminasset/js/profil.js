834d0276ee7e6ac80d77ddbba04be7b3b87e8adf
Jamshid <kalandarovjamshid01@gmail.com>
2022-09-22 05:42:20 +0500

    profil is end

--- public/adminasset/js/profil.js
+++ public/adminasset/js/profil.js
@@ -22,6 +22,28 @@ const update_user = async (name, username, email, photo) => {
   }
 };
 console.log('hello');
+const update_Password = async (password, newPassword, passwordConfirm) => {
+  try {
+    const res = await axios({
+      method: 'Patch',
+      url: 'http://127.0.0.1:8000/api/v1/users/updatePassword',
+      data: {
+        password,
+        newPassword,
+        passwordConfirm,
+      },
+    });
+    if (res.status === 200) {
+      console.log('hello');
+      alert('You logged successfully');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1000);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 document.querySelector('#userData').addEventListener('submit', (e) => {
   console.log('salom');
@@ -31,4 +53,13 @@ document.querySelector('#userData').addEventListener('submit', (e) => {
   const email = document.querySelector('#email_user').value;
   update_user(name, username, email);
   console.log(name, username, email);
+});
+
+document.querySelector('#userPassword').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const password = document.querySelector('#password_user').value;
+  const newPassword = document.querySelector('#newPassword_user');
+  const passwordConfirm = document.querySelector('#newPass_Confirm_user');
+  update_Password(password, newPassword, passwordConfirm);
+  console.log(password);
 });
