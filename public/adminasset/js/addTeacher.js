be0e3c9034860b9a684159bd765ff556daa6c9ae
Jamshid <kalandarovjamshid01@gmail.com>
2022-09-22 05:52:19 +0500

    teacher add and edit has been ready

--- public/adminasset/js/addTeacher.js
+++ public/adminasset/js/addTeacher.js
@@ -1,4 +1,4 @@
-const teacherAdd = async (
+const addTeacherFunc = async (
   name,
   username,
   email,
@@ -31,21 +31,21 @@ const teacherAdd = async (
   }
 };
 
-let addTeacher = document.querySelector('.addTeacher');
-let editTeacher = document.querySelector('.editTeacher');
-addTeacher.addEventListener('click', (e) => {
+let addTeacherBtn = document.querySelector('.addTeacher');
+let editTeacherBtn = document.querySelector('.editTeacher');
+addTeacherBtn.addEventListener('click', (e) => {
   document.querySelector('.addTeacherForm').classList.toggle('d-none');
 });
 document.querySelector('.addForm').addEventListener('submit', (e) => {
   e.preventDefault();
-  const name = document.querySelector('#name').value;
-  const username = document.querySelector('#username').value;
-  const email = document.querySelector('#email').value;
-  const password = document.querySelector('#password');
-  const passwordConfirm = document.querySelector('#passwordConfirm').value;
-  const photo = document.querySelector('#formFile').value;
-  const subjectId = document.querySelector('#subjectId');
-  teacherAdd(
+  const name = document.querySelector('#name_add').value;
+  const username = document.querySelector('#username_add').value;
+  const email = document.querySelector('#email_add').value;
+  const password = document.querySelector('#password_add');
+  const passwordConfirm = document.querySelector('#passwordConfirm_add').value;
+  const photo = document.querySelector('#formFile_add').value;
+  const subjectId = document.querySelector('#subjectId_add');
+  addTeacherFunc(
     name,
     username,
     email,
@@ -54,14 +54,58 @@ document.querySelector('.addForm').addEventListener('submit', (e) => {
     photo,
     subjectId
   );
-
   document.querySelector('.addTeacherForm').classList.toggle('d-none');
+  document.querySelector('.editTeacherForm').classList.add('d-none');
 });
-editTeacher.addEventListener('click', () => {
+
+const editTeacherFunc = async (
+  name,
+  username,
+  email,
+  photo,
+  password,
+  subjectId
+) => {
+  try {
+    const res = await axios({
+      method: 'PATCH',
+      url: 'http://127.0.0.1:8000/api/v1/users/:id',
+      data: {
+        name,
+        username,
+        email,
+        photo,
+        password,
+        subjectId: [subjectId],
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+editTeacherBtn.addEventListener('click', () => {
   console.log('hello');
   document.querySelector('.editTeacherForm').classList.toggle('d-none');
+  document.querySelector('.addTeacherForm').classList.add('d-none');
 });
 document.querySelector('.editForm').addEventListener('submit', (e) => {
   e.preventDefault();
+  const name = document.querySelector('#name_edit').value;
+  const username = document.querySelector('#username_edit').value;
+  const email = document.querySelector('#email_edit').value;
+  const password = document.querySelector('#password_edit');
+  const passwordConfirm = document.querySelector('#passwordConfirm_edit').value;
+  const photo = document.querySelector('#formFile_edit').value;
+  const subjectId = document.querySelector('#subjectId_edit');
+  addTeacherFunc(
+    name,
+    username,
+    email,
+    password,
+    passwordConfirm,
+    photo,
+    subjectId
+  );
   document.querySelector('.editTeacherForm').classList.toggle('d-none');
 });
