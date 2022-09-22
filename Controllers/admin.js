1c4d1d0adbd5cb7f3b987d14657bf9251e032a3f
mironshoh1603 <mironshohasadov1603@gmail.com>
2022-09-22 18:02:32 +0500

    finished book.pug

--- Controllers/admin.js
+++ Controllers/admin.js
@@ -34,7 +34,12 @@ const kitoblar = catchErrorAsync(async (req, res, next) => {
   let books = await axios('http://localhost:8000/api/v1/files/');
   books = books.data.data;
   let teachers = [];
-
+  let teacherDatas = await axios(
+    'http://localhost:8000/api/v1/users?role=teacher'
+  );
+  teacherDatas = teacherDatas.data.data;
+  let subjects = await axios('http://localhost:8000/api/v1/subjects/');
+  subjects = subjects.data.data;
   books.map((val) => {
     let variable = val.teacherId || {
       name: 'Nimajon Nimayev',
@@ -43,11 +48,11 @@ const kitoblar = catchErrorAsync(async (req, res, next) => {
     };
     teachers.push(variable);
   });
-  res.render('admin/book', { books, teachers });
+  res.render('admin/book', { books, teachers, teacherDatas, subjects });
 });
 
 const subject = catchErrorAsync(async (req, res, next) => {
-  console.log('mana');
+  // console.log('mana');
   let subjects = await axios('http://localhost:8000/api/v1/subjects/');
   subjects = subjects.data.data;
   let teachers = [];
